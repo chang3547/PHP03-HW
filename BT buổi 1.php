3b965cc36f6a193e29aedc5bd50ef4ac7d4757cc
chang3547 <trangdq3107@gmail.com>
2024-08-21 17:04:23 +0700

    first commit

---
+++ BT buổi 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+< lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title> Document </title>
+    </head>
+
+
+<body>
+<?php
+echo '<p style="text-align: center; color: blue">Bai tap lop PHP3 buoi 1 </p>';
+?>
+</body>
+</html>
